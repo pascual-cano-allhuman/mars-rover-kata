bc6b513ef49ff8228e2a8ece8facaa503077e2b3
Pascual Cano <pascual@allhuman.com>
2023-02-23 13:15:34 +0100

    Add a spy to test move action delegation

--- tests/rover.spec.ts
+++ tests/rover.spec.ts
@@ -1,11 +1,20 @@
 import { getRoverFinalPosition, Position, Direction, Command } from "../src/rover";
+const Rover = require("../src/rover");
 
 describe("Rover Kata tests", () => {
+	const position = { x: 1, y: 1 } as Position;
+	const direction = Direction.north;
+
 	it("goes to 1,2 when moving north from 1,1", () => {
-		const position = { x: 1, y: 1 } as Position;
-		const direction = Direction.north;
 		const commands = [Command.forward];
 		const newPosition = getRoverFinalPosition(position, direction, commands);
 		expect(newPosition).toEqual({ x: 1, y: 2 });
+	});
+
+	it("makes use of moveForwardOrBackward for move commands", () => {
+		const jestSpy = jest.spyOn(Rover, "moveForwardOrBackward");
+		const commands = [Command.forward, Command.backward];
+		getRoverFinalPosition(position, direction, commands);
+		expect(jestSpy).toHaveBeenCalledTimes(2);
 	});
 });
