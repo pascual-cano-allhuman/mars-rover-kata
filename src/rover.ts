d351ebb2778e3027f30e12bb6c0d755d5040623b
Pascual Cano <pascual@allhuman.com>
2023-02-24 12:15:33 +0100

    Split group

--- src/rover.ts
+++ src/rover.ts
@@ -29,6 +29,24 @@ export const isThereObstacle = (position: Position): boolean => {
 	return OBSTACLE_COORDS[position.x][position.y] === 1;
 };
 
-export const getRoverFinalPosition = (position: Position, direction: Direction, commands: Command[]): Position | null => {
-	return null;
+export const getRoverFinalPosition = (position: Position, direction: Direction, commands: Command[]): Position => {
+	let currentPosition = position;
+	let currentDirection = direction;
+	commands.forEach(command => {
+		currentPosition = move(currentPosition, direction, command);
+	});
+	return currentPosition;
+};
+
+export const move = (currentPosition: Position, direction: Direction, command: Command) => {
+	const { x, y } = currentPosition;
+	switch (direction) {
+		case Direction.north:
+			currentPosition = { x, y: y + 1 } as Position;
+			break;
+		case Direction.south:
+			currentPosition = { x, y: y - 1 } as Position;
+			break;
+	}
+	return currentPosition;
 };
--- tests/rover.spec.ts
+++ tests/rover.spec.ts
@@ -1,11 +1,48 @@
-import { getRoverFinalPosition, Position, Direction, Command } from "../src/rover";
+import { getRoverFinalPosition, move, Position, Direction, Command } from "../src/rover";
+import * as Rover from "../src/rover";
 
-describe("Rover Kata tests", () => {
-	it("Dummy test", () => {
-		const position = { x: 0, y: 0 } as Position;
+describe("Sequence of commands", () => {
+	it("goes to 1,2 when moving north once from 1,1", () => {
+		const position = { x: 1, y: 1 } as Position;
 		const direction = Direction.north;
 		const commands = [Command.forward];
 		const newPosition = getRoverFinalPosition(position, direction, commands);
-		expect(true).toBe(true);
+		expect(newPosition).toEqual({ x: 1, y: 2 });
+	});
+	it("goes to 1,0 when moving south once from 1,1", () => {
+		const position = { x: 1, y: 1 } as Position;
+		const direction = Direction.south;
+		const commands = [Command.forward];
+		const newPosition = getRoverFinalPosition(position, direction, commands);
+		expect(newPosition).toEqual({ x: 1, y: 0 });
+	});
+	it("goes to 1,3 when moving north twice from 1,1", () => {
+		const position = { x: 1, y: 1 } as Position;
+		const direction = Direction.north;
+		const commands = [Command.forward, Command.forward];
+		const newPosition = getRoverFinalPosition(position, direction, commands);
+		expect(newPosition).toEqual({ x: 1, y: 3 });
+	});
+	it("check that move has been called", () => {
+		const jestSpy = jest.spyOn(Rover, "move");
+		const position = { x: 1, y: 1 } as Position;
+		const direction = Direction.north;
+		const commands = [Command.forward, Command.forward];
+		getRoverFinalPosition(position, direction, commands);
+		expect(jestSpy).toHaveBeenCalledTimes(2);
+	});
+});
+
+describe("Move commands", () => {
+	const position = { x: 1, y: 1 } as Position;
+	it("goes to 1,2 when moving north from 1,1", () => {
+		const direction = Direction.north;
+		const newPosition = move(position, direction, Command.forward);
+		expect(newPosition).toEqual({ x: 1, y: 2 });
+	});
+	it("goes to 1,0 when moving south from 1,1", () => {
+		const direction = Direction.south;
+		const newPosition = move(position, direction, Command.forward);
+		expect(newPosition).toEqual({ x: 1, y: 0 });
 	});
 });
